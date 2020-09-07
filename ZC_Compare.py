777ae5740826248f3a57a2d056e4c63f8742ea2b
Paul Romano <dudeacough@gmail.com>
2020-09-06 23:26:10 -0700

    Commented

---
+++ ZC_Compare.py
@@ -0,0 +1,19 @@
+# python3
+# ZC_Compare.py
+# Program will compare a rental listing on Zillow with craiglist rental listings.  Duplicates on craiglist will be
+# provided as an output
+
+
+# TODO: Add Library Imports
+
+
+# TODO: Check if URL provided in sys arguments, otherwise look at clipboard.
+
+# TODO: Open Zillow site and capture the zip code and store photo images in an image object
+
+# TODO: Navigate to Craiglist and perform rental search based on zipcode from Zillow site
+
+# TODO: Cycle through each search results pulled up.  Navigate to next page if necessary
+
+# TODO: At each Craiglist listing, download images and compare them to the Zillow images.
+# if greater than a certain % similiar, then output the CL URL to a file
